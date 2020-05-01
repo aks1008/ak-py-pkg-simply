773323f613c6c524935d1ad317d74adc5233dbcd
Akshay <lungareakshay@gmail.com>
2020-05-02 01:43:53 +0530

    Change version number.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'AK_PY_PKG_SIMPLY',         # How you named your package folder (MyLib)
   packages = ['AK_PY_PKG_SIMPLY'],   # Chose the same as "name"
-  version = '0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'SIMPLFY TEXT',   # Give a short description about your library
   author = 'AKSHAY LUNGARE',                   # Type in your name
