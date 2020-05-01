d82ed58b14f4ec08ca0a68146f58196e4201fea5
Akshay <lungareakshay@gmail.com>
2020-05-02 01:17:32 +0530

    Added release url.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
   author = 'AKSHAY LUNGARE',                   # Type in your name
   author_email = 'lungareakshay@gmial.com',      # Type in your E-Mail
   url = 'https://github.com/lungareakshay/ak-py-pkg-simply.git',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/lungareakshay/ak-py-pkg-simply/archive/0.1.tar.gz',    # I explain this later on
   keywords = ['SIMPLFY TEXT'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
         #   'validators',
